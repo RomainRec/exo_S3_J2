76865d95f9d29f4f9f983050b15fd3ada178a34d
RomainRec <romain.recher@gmail.com>
2018-10-16 16:13:01 +0200

    presque fini

--- morpion.rb
+++ morpion.rb
@@ -1,17 +1,17 @@
 require "pry"
 
 class BoardCase
-  attr_accessor :valeur, :case_number
+  attr_accessor :value, :case_number
   #TO DO : la classe a 2 attr_accessor, sa valeur (X, O, ou vide), ainsi que son numéro de case)
 
   def initialize
-    @valeur = valeur
+    @value = value
     @case_number = case_number
     #TO DO doit régler sa valeur, ainsi que son numéro de case
   end
   
   def to_s
-    @valeur.to_s #TO DO : doit renvoyer la valeur au format string
+    @value.to_s #TO DO : doit renvoyer la valeur au format string
   end
 
 end
@@ -19,6 +19,7 @@ end
 class Board
   include Enumerable
   attr_accessor :array
+  @@count = 1
   #TO DO : la classe a 1 attr_accessor, une array qui contient les BoardCases
 
 
@@ -33,14 +34,107 @@ class Board
     @c2 = BoardCase.new
     @c3 = BoardCase.new
 
-    @array = [@a1.valeur, @a2.valeur, @a3.valeur, @b1.valeur, @b2.valeur, @b3.valeur, @c1.valeur, @c2.valeur, @c3.valeur]
+    @array = [@a1.value, @a2.value, @a3.value, @b1.value, @b2.value, @b3.value, @c1.value, @c2.value, @c3.value]
     
     #TO DO :
     #Quand la classe s'initialize, elle doit créer 9 instances BoardCases
     #Ces instances sont rangées dans une array qui est l'attr_accessor de la classe
   end
 
+ def player_one
+    # Méthode qui change la valeur ("", 0 ou X) de la BoardCase en fonction du choix du joueur qui a les X pour refléter sont coup
+    puts "joueur 1 choisis une case:"
+    x = gets.chomp.to_i
+    case x
+      when 1 
+          @a1.value = "X" 
+        display
+
+      when 2 
+        @a2.value = "X"
+          display
+      when 3 
+        @a3.value = "X"
+          display
+      when 4 
+        @b1.value = "X"
+          display
+      when 5 
+        @b2.value = "X"
+          display
+      when 6 
+        @b3.value = "X"
+          display
+      when 7 
+        @c1.value = "X"
+          display
+      when 8 
+        @c2.value = "X"
+          display
+      when 9 
+        @c3.value = "X"
+          display
+      else puts "la case est déjà prise"
+        player_one
+      end
+    victory? # on vérifie si une combinaison gagnante a été obtenue
+    @@count += 1
+    if @@count <= 9
+    player_two # on passe au joueur 2
+  else 
+    display
+    puts "Egalité!" #Cas où les 9 coups ont été joués sans qu'il n'y ait de vainqueur
+  end
+end
+
+  def player_two
+    # # Méthode qui change la valeur ("", 0 ou X) de la BoardCase en fonction du choix du joueur qui a les 0 pour refléter sont coup
+    puts "joueur 2 choisis une case:"
+    x = gets.chomp.to_i
+    case x
+      when 1 
+        @a1.value = "O"
+          display
+      when 2 
+        @a2.value = "O"
+          display
+      when 3 
+        @a3.value = "O"
+          display
+      when 4
+        @b1.value = "O"
+          display
+      when 5 
+        @b2.value = "O"
+          display
+      when 6
+        @b3.value = "O"
+          display
+      when 7 
+        @c1.value = "O"
+          display
+      when 8
+        @c2.value = "O"
+          display
+      when 9 
+        @c3.value = "O"
+          display
+      else puts "la case est déjà prise"
+        player_two
+      end
+    victory? # on vérifie si une combinaison gagnate a été obtenue
+    @@count += 1
+    if @@count <= 9
+    player_one # on repasse au joueur 1
+  else
+    display #Cas où les 9 coups ont été joués sans qu'il n'y ait de vainqueur
+    puts "Egalité!"
+  end
+end
+  
+
   def display
+   
     puts "_________"
     puts "|#{@a1} | #{@a2} | #{@a3}|"
     puts "|_______|"
@@ -54,18 +148,20 @@ class Board
 
   def victory?
       # LE X GAGNE 
-      if array[0] == "X" && array[1] == "X" && array[2] == "X" || array[3] == "X" && array[4] == "X" && array[5] == "X"||
-         array[6] == "X" && array[7] == "X" && array[8] == "X" || array[0] == "X" && array[3] == "X" && array[6] == "X"||
-         array[1] == "X" && array[4] == "X" && array[7] == "X" || array[2] == "X" && array[5] == "X" && array[8] == "X"||
-         array[0] == "X" && array[4] == "X" && array[8] == "X" || array[6] == "X" && array[4] == "X" && array[2] == "X"
-      return true
+      if @a1.value == "X" && @a2.value == "X" && @a3.value == "X" || @a1.value == "X" && @b2.value == "X" && @c3.value == "X"||
+         @a1.value == "X" && @b1.value == "X" && @c1.value == "X" || @a2.value == "X" && @b2.value == "X" && @c2.value == "X"||
+         @a3.value == "X" && @b3.value == "X" && @c3.value == "X" || @b1.value == "X" && @b2.value == "X" && @b3.value == "X"||
+         @c1.value == "X" && @c2.value == "X" && @c3.value == "X" || @c1.value == "X" && @b2.value == "X" && @a3.value == "X"
+      puts "VICTORYYYYY ! "
+      exit
       end
       # LE O GAGNE 
-      if array[0] == "O" && array[1] == "O" && array[2] == "O" || array[3] == "O" && array[4] == "O" && array[5] == "O"||
-         array[6] == "O" && array[7] == "O" && array[8] == "O" || array[0] == "O" && array[3] == "O" && array[6] == "O"||
-         array[1] == "O" && array[4] == "O" && array[7] == "O" || array[2] == "O" && array[5] == "O" && array[8] == "O"||
-         array[0] == "O" && array[4] == "O" && array[8] == "O" || array[6] == "O" && array[4] == "O" && array[2] == "O"
-      return true
+      if @a1.value == "O" && @a2.value == "O" && @a3.value == "O" || @a1.value == "O" && @b2.value == "O" && @c3.value == "O"||
+         @a1.value == "O" && @b1.value == "O" && @c1.value == "O" || @a2.value == "O" && @b2.value == "O" && @c2.value == "O"||
+         @a3.value == "O" && @b3.value == "O" && @c3.value == "O" || @b1.value == "O" && @b2.value == "O" && @b3.value == "O"||
+         @c1.value == "O" && @c2.value == "O" && @c3.value == "O" || @c1.value == "O" && @b2.value == "O" && @a3.value == "O"
+      puts "VICTORYYYYY ! "
+      exit
       end
   end
 end
@@ -108,59 +204,27 @@ class Game
     # TO DO : lance la partie
   #end
 
-  def turn 
+  def lancement
       #affiche le tableau
-    puts "_____________"
-    puts "| 1 | 2 | 3 |"
-    puts "|-----------|"
-    puts "| 4 | 5 | 6 |"
-    puts "|-----------|"
-    puts "| 7 | 8 | 9 |"
-    puts "-------------"
+      puts "LE JEU COMMENCE !!!!"
+      puts "Voici le plateau de jeu : "
+      puts "_____________"
+      puts "| 1 | 2 | 3 |"
+      puts "|-----------|"
+      puts "| 4 | 5 | 6 |"
+      puts "|-----------|"
+      puts "| 7 | 8 | 9 |"
+      puts "-------------"
+  lancement = Board.new
+  lancement.player_one
 
-    victoire = Board.new
-    test_victoire = victoire.victory?
-
-    until test_victoire == true
-      puts "A toi de jouer, choisi le numéro de ta case"
-      x = gets.chomp.to_i
-      tour = Board.new
-      tour1 = tour.play
-
-      #@array[0] = 1
-      #@array[1] = 2
-      #@array[2] = 3
-      #@array[3] = 4
-      #@array[4] = 5
-      #@array[5] = 6
-      #@array[6] = 7
-      #@array[7] = 8
-      #@array[8] = 9
-
-end
-def play
-      i=0
-      until i > 8
-        if @array[i] == nil
-           puts @array[i] = @valeur 
-        elsif  
-          @array[i] == @valeur  
-           puts "recommence"
-        end
-         i += 1
-      end
   end
-
-    end
-
-
 
     #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
-  end
+  
 
-  end
 
 #Game.new.go
-
+end
 binding.pry 
 
